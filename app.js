4cd5cd1c6f4a01e9fba25135a57ba78708d5763c
jamesnathanan <artofgems@live.com>
2021-09-22 19:54:07 +0700

    fix some issue typeerror about n thing

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ client.connect(function (err) {
 
   const db = client.db(dbName);
 
-  client.close();
+  findDocuments(db, function () {
+    client.close();
+  });
 });
 
 const insertDocuments = function (db, callback) {
@@ -32,10 +34,26 @@ const insertDocuments = function (db, callback) {
     ],
     function (err, result) {
       assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
+
+      assert.equal(3, result.insertedCount);
+
+      assert.equal(3, Object.keys(result.insertedIds).length);
+
       console.log("Inserted 3 documents into the collection");
+
       callback(result);
     }
   );
+};
+
+const findDocuments = function (db, callback) {
+  // Get the documents collection
+  const collection = db.collection("fruits");
+  // Find some documents
+  collection.find({}).toArray(function (err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits);
+    callback(fruits);
+  });
 };
